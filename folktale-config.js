6cef53e49e150a824ac3dd2fe4354ba93702f119
Dave Falke <dmfalke@gmail.com>
2017-01-08 23:07:44 -0500

    Add root folktale package

--- folktale-config.js
+++ folktale-config.js
@@ -2,21 +2,22 @@
 window.process = { env: { } }
 
 var packages = [
-  'core',
-  'data',
-  'core/adt',
-  'core/fantasy-land',
-  'core/lambda',
-  'core/object',
-  'data/conversions',
-  'data/either',
-  'data/future',
-  'data/maybe',
-  'data/task',
-  'data/validation',
+  '',
+  '/core',
+  '/data',
+  '/core/adt',
+  '/core/fantasy-land',
+  '/core/lambda',
+  '/core/object',
+  '/data/conversions',
+  '/data/either',
+  '/data/future',
+  '/data/maybe',
+  '/data/task',
+  '/data/validation',
 ]
 .reduce((packages, entry) => Object.assign(packages, {
-  [`folktale/${entry}`]: {
+  [`folktale${entry}`]: {
     main: 'index'
   }
 }), {})
